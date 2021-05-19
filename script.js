213c9f299ad8d9cbe7cf15cd074d19a18cdc9cb1
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2021-05-18 23:56:02 -0300

    First commit

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+$('article')
+.html('Click me') //Try edit it...
+.on('click', () =>
+    window.alert(`Olá Seja Bem Vindo ${nome},  esse apenas um projeto estático para ver mais projetos como esse acesse meu portfólio https://github.com/wilkerlisboa`))
 
+console.log('Funcionou')
 
+let nome = window.prompt('Olá Qual e seu nome?  (obrigatório)'); // ira pergunta o nome de uma person
+
+window.alert(`Olá ${nome}, queria de informar que não havera captura de nem um tipo dado como por exemplo seu nome.`) // ira fazer uma preve informação com o nome da person 
